111f9db191652d78e2f037059a2b0b64cc9f058c
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-08-04 14:38:43 +0600

    Create PythonProgram56.py

---
+++ PythonProgram56.py
@@ -0,0 +1,66 @@
+# Introduction to OOP : Class and object
+
+class Student:
+    roll = ""
+    gpa = ""
+
+
+rahim = Student()
+rahim.roll = 101
+rahim.gpa = 3.70
+print(f"Roll : {rahim.roll}, GPA : {rahim.gpa}")
+
+
+karim = Student()
+karim.roll = 102
+karim.gpa = 3.60
+print(f"Roll : {karim.roll}, GPA : {karim.gpa}")
+
+
+tarim = Student()
+tarim.roll = 103
+tarim.gpa = 3.61
+print(f"Roll : {tarim.roll}, GPA : {tarim.gpa}")
+
+
+yarim = Student()
+yarim.roll = 104
+yarim.gpa = 3.62
+print(f"Roll : {yarim.roll}, GPA : {yarim.gpa}")
+
+
+jarim = Student()
+jarim.roll = 105
+jarim.gpa = 3.63
+print(f"Roll : {jarim.roll}, GPA : {jarim.gpa}")
+
+
+marim = Student()
+marim.roll = 106
+marim.gpa = 3.64
+print(f"Roll : {marim.roll}, GPA : {marim.gpa}")
+
+
+varim = Student()
+varim.roll = 107
+varim.gpa = 3.65
+print(f"Roll : {varim.roll}, GPA : {varim.gpa}")
+
+
+zarim = Student()
+zarim.roll = 108
+zarim.gpa = 3.66
+print(f"Roll : {zarim.roll}, GPA : {zarim.gpa}")
+
+
+sarim = Student()
+sarim.roll = 109
+sarim.gpa = 3.67
+print(f"Roll : {sarim.roll}, GPA : {sarim.gpa}")
+
+
+farim = Student()
+farim.roll = 110
+farim.gpa = 3.68
+print(f"Roll : {farim.roll}, GPA : {farim.gpa}")
+
